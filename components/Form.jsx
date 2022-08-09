14ea389a1a0d10a8b8236ae78ae420974438aa34
jonarroh <jonarrodi99@gmail.com>
2022-08-09 11:00:06 -0500

    🛠️ the service is working

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,31 +1,42 @@
 import {
+	background,
 	Box,
 	Button,
 	Center,
 	Container,
 	Input,
-	Text
+	useClipboard
 } from '@chakra-ui/react';
 import { useRef, useState } from 'react';
+import Swal from 'sweetalert2';
+import { shortlinkServices } from '../services/shortLink';
+
 import ShortText from './ShortText';
 
 const Form = () => {
 	const inputRef = useRef();
-	const [link, setLink] = useState('');
+	const [shortLink, setShortLink] = useState('');
+	const { hasCopied, onCopy } = useClipboard(shortLink);
+	const BASE_URL = '';
+	if (process.env.NODE_ENV === 'development') {
+		BASE_URL = 'http://localhost:3000';
+	}
+	BASE_URL = 'https://short-link-jonarroh.vercel.app';
 
-	const handleSubmit = e => {
+	const handleSubmit = async e => {
 		e.preventDefault();
 		const url = inputRef.current.value;
-		fetch('./api/shortLink', {
-			method: 'POST',
-			Headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(url)
-		})
-			.then(res => res.json())
-
-			.then(data => {
-				setLink(`short-link-jonarroh.vercel.app/${data.shortUrl}`);
-			});
+		if (!url) return;
+		const data = await shortlinkServices(url);
+		const completeUrl = `${BASE_URL}/${data.shortUrl}`;
+		inputRef.current.value = completeUrl;
+		setShortLink(completeUrl);
+		Swal.fire({
+			title: 'Short Link Created',
+			icon: 'success',
+			background: '#1A202C',
+			confirmButtonText: 'Copy link'
+		});
 	};
 	return (
 		<>
@@ -52,6 +63,9 @@ const Form = () => {
 									variant="solid"
 									type="submit">
 									Shorten
+								</Button>
+								<Button onClick={onCopy} ml={2}>
+									{hasCopied ? 'Copied' : 'Copy'}
 								</Button>
 							</Center>
 						</Box>
